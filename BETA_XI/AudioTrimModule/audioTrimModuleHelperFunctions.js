1ec653de31617e309f35e4c8a74f32da271a7be8
daniel0130 <stigerpanda@gmail.com>
2024-09-13 16:12:32 +0100

    working apart from css issues

--- BETA_XI/AudioTrimModule/audioTrimModuleHelperFunctions.js
+++ BETA_XI/AudioTrimModule/audioTrimModuleHelperFunctions.js
@@ -41,7 +41,6 @@ function stopAudioForChannel(channelIndex) {
 }
 
 // Initialize audio trimmer modal with channel-specific settings
-// Initialize audio trimmer modal with channel-specific settings
 function initializeAudioTrimmer(channelIndex) {
     currentTrimmerChannelIndex = channelIndex;
 
